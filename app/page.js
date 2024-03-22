6e7b9257eece246cdaf28df8a8161d971c103708
Ayman Kaddar <71832197+Ayman-Kaddar@users.noreply.github.com>
2024-03-22 20:52:05 +0100

    awda

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
-import HomePage from "@/components/views/homePage";
+import Header from '@/components/molecules/header/header';
+import Hero from '@/components/molecules/hero/hero';
 
-export default HomePage
+export default function Home() {
+    return (
+        <>
+          <Header />
+          <div className="flex flex-col w-full h-auto items-center">
+            <section className="flex flex-col w-full h-auto items-center mt-32 px-5 pb-24 z-20">
+              <Hero />
+            </section>
+          </div>
+        </>
+      );
+    };
